3837021ba3e4c3437dfc2d673c9fdbdc839779c8
Bloomsburg Cyber Defense Club <dp92071@huskies.bloomu.edu>
2015-09-11 23:08:58 -0400

    second commit

---
+++ server.js
@@ -0,0 +1,27 @@
+var http = require("http");
+var fs = require("fs");
+
+function send404Response(response)
+{
+	response.writeHead(404, {"Context-Type": "Text/plain"});
+	response.write("Error 404: Page not found!");
+	response.end();
+	
+}
+
+function onRequest(request, response)
+{
+	console.log("Request")
+
+	if (request.method == "GET" && (request.url == "/" || request.ur == "/index.html")) 
+	{
+		response.writeHead(200, {"Context-Type": "Text/plain"})
+		fs.createReadStream("./index.html").pipe(response);
+	} else {
+		send404Response(response);
+		console.log("Cannot find requested page: " + request.url);
+	}
+}
+
+http.createServer(onRequest).listen(3000);
+console.log("Server is now running...");
